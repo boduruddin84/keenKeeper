c41b48ad4a03b299491879557080ce8b4a81d82b
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-04-15 16:51:45 +0600

    Loading spinner added

--- src/homepage/YourFriends.jsx
+++ src/homepage/YourFriends.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import FriendsCard from "../components/ui/FriendsCard";
+import { HashLoader } from "react-spinners";
 
 const YourFriends = () => {
   const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFriendsData = async () => {
       const res = await fetch("/data.json");
       const friendsDetailsData = await res.json();
       setFriends(friendsDetailsData);
+      setLoading(false);
     };
     fetchFriendsData();
   }, []);
@@ -19,7 +22,10 @@ const YourFriends = () => {
       <div className="w-fit lg:w-6xl mx-auto mb-4">
         <h3 className="text-2xl font-semibold text-[#1F2937]">Your Friends</h3>
       </div>
-      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-fit lg:w-6xl mx-auto">
+      {
+        loading ? (<div className="flex justify-center items-center">
+            <HashLoader />
+        </div> ) : ( <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-fit lg:w-6xl mx-auto">
         {friends.map((friend) => {
           return (
             <div key={friend.id}>
@@ -28,6 +34,7 @@ const YourFriends = () => {
           );
         })}
       </div>
+    )}
     </div>
   );
 };
