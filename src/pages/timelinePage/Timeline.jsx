bd59d7cb569f4f5824c1692ea0b2da4861a59e89
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-04-16 09:43:26 +0600

    Timeline page added

--- src/pages/timelinePage/Timeline.jsx
+++ src/pages/timelinePage/Timeline.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Timeline = () => {
   return (
-    <div>Timeline</div>
-  )
-}
+    <div className="bg-[#F8FAFC]">
+      <div className="w-6xl mx-auto">
+        <div>
+          <h2 className="text-5xl text-[#1F2937] font-bold pt-20 pb-10">Timeline</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Timeline
+export default Timeline;
